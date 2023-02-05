5e3a4fd7c1f9259978c0b662fc772f3a15763755
rossboehme <ross.boehme@gmail.com>
2023-02-05 15:10:08 -0500

    SQL to generate data for DATA607 assignment 2

---
+++ movie_info.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE `survey`.`movie_info` 
+(
+    movie_id	INT,
+    movie_genre	VARCHAR(512),
+    movie_name	VARCHAR(512)
+);
+
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('1', 'Action', 'Black Panther: Wakanda Forever');
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('2', 'Drama', 'Tar');
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('3', 'Horror', 'M3GAN');
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('4', 'Action', 'Top Gun: Maverick');
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('5', 'Science Fiction', 'Avatar: The Way of the Water');
+INSERT INTO survey.movie_info (movie_id, movie_genre, movie_name) VALUES ('6', 'Adventure', 'Jurassic World: Dominion');
